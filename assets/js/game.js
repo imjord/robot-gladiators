24fa42a0c382a096f24214e7ef0394a9e3b95d19
imjord <dean.jay.goat@gmail.com>
2021-05-03 18:03:22 -0600

    learned functions/var/objects/

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,74 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+//declared functions start !!!
+
+// this is a function with the name of fight and parenthese which is always needed.
+function fight() // start of code block the function named fight will only execute this block of code within the curly brackets
+{
+    window.alert("The fight has begun!");  // browser function alerting the player fight has begun 
+
+
+}
+
+
+
+
+
+// window function (window=browser)
+// window.prompt("What is your robot's name?");   // browser function asking for player input that is sent back to program, USER INPUT 
+
+
+
+
+
+
+
+var playerName = window.prompt("What is your robot's name?"); // can call on this var name by using playerName USER INPUT WILL BE PLACED INTO THE VAR playerName
+
+// what is this?
+console.log(playerName)
+
+
+
+console.log("This logs a string, good for leaving youself a message");
+// this will do math and log 20
+console.log(10 +10);
+// what is this?
+console.log("Our robots name is" + playerName);
+
+
+
+
+
+
+// window.alert(playerName);
+// note the lack of quotation marks around playerName
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fight();  // a call to the function we declared ^
